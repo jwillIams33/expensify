b29d13b3a801b78500f79685b3ea8a1b94759ffa
Williams <jamal.williams@wwt.com>
2020-04-19 21:26:37 -0500

    add additional account bal

--- src/components/GrandBalance.js
+++ src/components/GrandBalance.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import {TotalBalance} from '../data/TotalBalance'
+import React from "react";
+import { TotalBalance } from "../data/TotalBalance";
 
 export default function GrandBalance() {
-    return (
-    <h3>{TotalBalance}</h3>
-    )
+  return (
+    <>
+      <h2>Grand Balance: {TotalBalance}</h2>
+      <h3>Account 1: $4000 </h3>
+      <h3>Account 2: $5000</h3>
+      <h3>Account 3: $6000</h3>
+    </>
+  );
 }
